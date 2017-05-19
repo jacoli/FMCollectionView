590d9fdb4088753ccbdd5f72a5281aba72e2baf8
jacoli <jaco.lcg@gmail.com>
2017-05-19 09:51:50 +0800

    update examples

--- Examples/Examples/FrameStyleViewController.m
+++ Examples/Examples/FrameStyleViewController.m
@@ -12,6 +12,8 @@
 @interface FrameStyleViewController () <FMCollectionViewDelegatesAndDataSource>
 
 @property (nonatomic, strong) FMCollectionView *collectionView;
+@property (nonatomic, strong) NSMutableArray *layout1;
+@property (nonatomic, strong) NSArray *layout2;
 
 @end
 
@@ -29,6 +31,31 @@
     self.collectionView.delegatesAndDataSource = self;
     self.collectionView.editingMode = YES;
     [self.view addSubview:self.collectionView];
+    
+    CGFloat width = CGRectGetWidth(self.collectionView.frame);
+    self.layout1 = [[NSMutableArray alloc] init];
+    [self.layout1 addObject:[NSValue valueWithCGRect:CGRectMake(0, 0, width * 0.4, width * 0.4)]];
+    [self.layout1 addObject:[NSValue valueWithCGRect:CGRectMake(width * 0.4 + 1, 0, width * 0.6 - 1, width * 0.2 - 0.5)]];
+    [self.layout1 addObject:[NSValue valueWithCGRect:CGRectMake(width * 0.4 + 1, width * 0.2 + 0.5, width * 0.6 - 1, width * 0.2 - 0.5)]];
+    
+    self.layout2 = [self buildLayoutStyle2];
+}
+
+- (NSArray *)buildLayoutStyle2 {
+    NSMutableArray *layout = [[NSMutableArray alloc] init];
+    
+    CGFloat width = CGRectGetWidth(self.view.frame);
+    CGFloat itemWidth = (width - 6.0) / 4.0;
+    CGFloat heightRatio = 0.4;
+    
+    [layout addObject:[NSValue valueWithCGRect:CGRectMake(0, 0, width * 0.5 - 1, width * heightRatio)]];
+    [layout addObject:[NSValue valueWithCGRect:CGRectMake(width * 0.5 + 1, 0, width * 0.5 - 1, width * heightRatio)]];
+    [layout addObject:[NSValue valueWithCGRect:CGRectMake((itemWidth + 2.0) * 0, width * heightRatio + 2, (width - 6.0) / 4.0, width * heightRatio)]];
+    [layout addObject:[NSValue valueWithCGRect:CGRectMake((itemWidth + 2.0) * 1, width * heightRatio + 2, (width - 6.0) / 4.0, width * heightRatio)]];
+    [layout addObject:[NSValue valueWithCGRect:CGRectMake((itemWidth + 2.0) * 2, width * heightRatio + 2, (width - 6.0) / 4.0, width * heightRatio)]];
+    [layout addObject:[NSValue valueWithCGRect:CGRectMake((itemWidth + 2.0) * 3, width * heightRatio + 2, (width - 6.0) / 4.0, width * heightRatio)]];
+    
+    return layout;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,16 +68,10 @@
 }
 
 - (CGRect)frameLayoutInCollectionView:(FMCollectionView *)collectionView frameOfItemInSectionAtIndexPath:(NSIndexPath *)indexPath sectionContentWidth:(CGFloat)inWidth {
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            return CGRectMake(0, 0, inWidth * 0.4, inWidth * 0.4);
-        } else if (indexPath.row == 1) {
-            return CGRectMake(inWidth * 0.4 + 1, 0, inWidth * 0.6 - 1, inWidth * 0.2 - 0.5);
-        } else {
-            return CGRectMake(inWidth * 0.4 + 1, inWidth * 0.2 + 0.5, inWidth * 0.6 - 1, inWidth * 0.2 - 0.5);
-        }
-    } else if (indexPath.section == 1) {
-        return CGRectMake(inWidth * 0.1 * indexPath.row, (inWidth * 0.4 + 1) * indexPath.row, inWidth * 0.4, inWidth * 0.4);
+    if (indexPath.section % 2 == 0) {
+        return [self.layout1[indexPath.row] CGRectValue];
+    } else if (indexPath.section % 2 == 1) {
+        return [self.layout2[indexPath.row] CGRectValue];;
     } else {
         return CGRectZero;
     }
@@ -61,10 +82,10 @@
 }
 
 - (NSInteger)collectionView:(FMCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    if (section == 0) {
-        return 3;
-    } else if (section == 1) {
-        return 5;
+    if (section % 2 == 0) {
+        return self.layout1.count;
+    } else if (section % 2 == 1) {
+        return self.layout2.count;
     } else {
         return 0;
     }
